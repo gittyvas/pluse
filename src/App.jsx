d6938a59981fdc428fe0e7bbe6ef88e3d65acc7e
gittyvas <jefffvasss@gmail.com>
2025-07-17 13:03:12 -0700

    About

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import About from './pages/About';
+import About from "./pages/About";
 import Home from "./pages/Home";
 import ContactsPage from "./pages/ContactsPage";
 import RemindersPage from "./pages/RemindersPage";
